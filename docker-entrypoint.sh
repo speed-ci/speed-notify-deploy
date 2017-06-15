7f7e190177d0dd805d3fa3198aed1bf45a49b0cb
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-06-15 12:18:59 +1100

    feat(message): post a test message to Rockchat

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,9 +18,23 @@ check_notify_env () {
     fi    
 }
 
-
 printmainstep "Notification du déploiement du macroservice sur Rocketchat"
 printstep "Vérification des paramètres d'entrée"
 
 init_env
 check_notify_env
+
+ROCKETCHAT_API_URL=$ROCKETCHAT_URL/api/v1
+
+LOGIN_RESULT=`curl -s --noproxy '*' $ROCKETCHAT_API_URL/login -d "username=$ROCKETCHAT_USER&password=$ROCKETCHAT_PASSWORD"`
+LOGIN_STATUS=`echo $LOGIN_RESULT | jq .status | tr -d '"'`
+if [[ "$LOGIN_STATUS" != "success" ]]; then 
+    printerror "Erreur de connection à $ROCKETCHAT_URL avec l'utilisateur $ROCKETCHAT_USER"
+    exit 1
+fi
+
+AUTH_TOKEN=`echo $LOGIN_RESULT | jq .data.authToken | tr -d '"'`
+USER_ID=`echo $LOGIN_RESULT | jq .data.userId | tr -d '"'`
+PROJECT_PREFIX=${PROJECT_NAME%-*}
+
+`curl --noproxy '*' --header "X-Auth-Token: $AUTH_TOKEN" --header "X-User-Id: $USER_ID" --header "Content-type:application/json"  $ROCKETCHAT_API_URL/chat.postMessage  -d '{ "channel": "#SLN_tests-rocketchat", "text": ":white_check_mark: L`application $PROJECT_PREFIX a été déployée avec succès\n $GITLAB_URL/$PROJECT_NAMESPACE/$PROJECT_NAME/builds/$CI_BUILD_ID" }' | jq`
