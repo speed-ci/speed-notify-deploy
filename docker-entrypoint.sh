a7eab8871f9a83f64eabe98edb0c598cfe2d3199
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-07-17 16:04:17 +1100

    chore(message): change message information order

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -70,7 +70,7 @@ CHAN_ENV=SLN_ENV_$ENV_NAME
 
 printstep "Envoi de la notification Rocketchat sur le chan $CHAN_APP"
 
-MSG="$NOTIFY_MENTION $EMOJI_STATUS Application *$APP_DISPLAY_NAME* déployée par *$USER_NAME* avec *$LABEL_STATUS* sur *$CI_ENVIRONMENT_NAME* (accès aux logs : $GITLAB_URL/$PROJECT_NAMESPACE/$PROJECT_NAME/builds/$CI_BUILD_ID)"
+MSG="$NOTIFY_MENTION $EMOJI_STATUS Application *$APP_DISPLAY_NAME* déployée avec *$LABEL_STATUS* par *$USER_NAME* sur *$CI_ENVIRONMENT_NAME* (accès aux logs : $GITLAB_URL/$PROJECT_NAMESPACE/$PROJECT_NAME/builds/$CI_BUILD_ID)"
 PAYLOAD=`jq --arg channel "#$CHAN_APP" --arg msg "$MSG" '. | .channel=$channel | .text=$msg' <<< '{}'`
 curl -s --noproxy '*' --header "X-Auth-Token: $AUTH_TOKEN" --header "X-User-Id: $USER_ID" --header "Content-type:application/json"  $ROCKETCHAT_API_URL/chat.postMessage  -d "$PAYLOAD" | jq .
 
