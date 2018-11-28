4c5c344257a010011c0ee2d23c8f380900682f64
Mathieu Filotto <m.filotto@eramet-sln.nc>
2018-11-29 09:49:11 +1100

    fix(url): remove -prod suffixe from url

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -45,12 +45,11 @@ PROJECT=${SERVICE_TO_UPDATE:-$PROJECT_PREFIX}
 GITLAB_USER_ID=${TRIGGER_USER_ID:-$GITLAB_USER_ID}
 
 if [[ $CI_ENVIRONMENT_URL != "" ]]; then
-    APP_DISPLAY_NAME="[$PROJECT]($CI_ENVIRONMENT_URL)"
+    ENVIRONMENT_URL=${CI_ENVIRONMENT_URL//-prod/}
+    APP_DISPLAY_NAME="[$PROJECT]($ENVIRONMENT_URL)"
 else
     APP_DISPLAY_NAME=$PROJECT
 fi
-
-
 
 PROJECT_ID=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_NAME" | jq --arg project_namespace "$PROJECT_NAMESPACE" '.[] | select(.namespace.name == "\($project_namespace)") | .id'`
 LAST_COMMIT_ID=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_ID/repository/commits?per_page=25&page=1&ref_name=$BRANCH_NAME" | jq -r '.[] | select(.message | startswith("Merge branch") | not) | .id' | head -1`
